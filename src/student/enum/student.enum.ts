cfe9330cce506d3d55c93c2008c60b133705a061
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-19 11:16:36 +0300

    added the student enum file

---
+++ src/student/enum/student.enum.ts
@@ -0,0 +1,19 @@
+export enum StudentClass {
+  SENIOR_ONE = 'Senior One',
+  SENIOR_TWO = 'Senior Two',
+  SENIOR_THREE = 'Senior Three',
+  SENIOR_FOUR = 'Senior Four',
+  SENIOR_FIVE = 'Senior Five',
+  SENIOR_SIX = 'Senior Six',
+}
+
+export enum StudentLevel {
+  O_LEVEL = 'O Level',
+  A_LEVEL = 'A Level',
+}
+
+export enum Gender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  OTHER = 'Other',
+}
