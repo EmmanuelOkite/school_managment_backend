72e16cc7dc7edc332f34008adac4962733ea545f
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-23 08:59:44 +0300

    added teachers enum file

---
+++ src/teacher/enum/teacher.enum.ts
@@ -0,0 +1,32 @@
+export enum EmploymentType {
+  FULL_TIME = 'Full-Time',
+  PART_TIME = 'Part-Time',
+  CONTRACT = 'Contract',
+}
+
+export enum TeacherStatus {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+  ON_LEAVE = 'On Leave',
+}
+
+export enum TeacherRole {
+  TEACHER = 'Teacher',
+  HEAD_TEACHER = 'Head Teacher',
+  ADMINISTRATOR = 'Administrator',
+}
+
+export enum Gender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  OTHER = 'Other',
+}
+
+export enum HighestQualification {
+  CERTIFICATE = 'Certificate',
+  DIPLOMA = 'Diploma',
+  BACHELORS = 'Bachelors Degree',
+  MASTERS = 'Masters Degree',
+  PHD = 'PhD',
+  OTHER = 'Other',
+}
