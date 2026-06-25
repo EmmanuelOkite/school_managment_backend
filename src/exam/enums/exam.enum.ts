25b838500acbb6df6112c7539beb908bd3f6877e
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-25 08:46:39 +0300

    added exams enum file

---
+++ src/exam/enums/exam.enum.ts
@@ -0,0 +1,21 @@
+export enum ExamStatus {
+  DRAFT = 'Draft',
+  SCHEDULED = 'Scheduled',
+  ONGOING = 'Ongoing',
+  COMPLETED = 'Completed',
+  CANCELLED = 'Cancelled',
+}
+
+export enum Term {
+  TERM_ONE = 'Term 1',
+  TERM_TWO = 'Term 2',
+  TERM_THREE = 'Term 3',
+}
+
+export enum GradingScale {
+  DISTINCTION = 'Distinction',
+  MERIT = 'Merit',
+  CREDIT = 'Credit',
+  PASS = 'Pass',
+  FAIL = 'Fail',
+}
