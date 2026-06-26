9b244a1b60d721b68431208c2e95da105a52ad3d
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-26 18:59:05 +0300

    added timetable module in app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { ParentModule } from './parent/parent.module';
 import { Parent } from './parent/entities/parent.entity';
 import { ExamModule } from './exam/exam.module';
 import { Exam } from './exam/entities/exam.entity';
+import { TimetableModule } from './timetable/timetable.module';
+import { Timetable } from './timetable/enums/entities/timetable.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -19,13 +21,14 @@ import { Exam } from './exam/entities/exam.entity';
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASSWORD || 'innovation2?',
       database: process.env.DB_NAME || 'school_db',
-      entities: [Student, Teacher, Parent, Exam],
+      entities: [Student, Teacher, Parent, Exam, Timetable],
       synchronize: true,
     }),
     StudentModule,
     TeacherModule,
     ParentModule,
     ExamModule,
+    TimetableModule,
   ],
   controllers: [AppController],
   providers: [AppService],
