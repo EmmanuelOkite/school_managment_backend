c11e8478872bbb608f2859459de667ae9c66efc9
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-26 18:50:29 +0300

    added timetable enums file

---
+++ src/timetable/enums/timetable.enum.ts
@@ -0,0 +1,20 @@
+export enum TimetableType {
+  CLASS = 'Class',
+  TEACHER = 'Teacher',
+  EXAM = 'Exam',
+}
+
+export enum DayOfWeek {
+  MONDAY = 'Monday',
+  TUESDAY = 'Tuesday',
+  WEDNESDAY = 'Wednesday',
+  THURSDAY = 'Thursday',
+  FRIDAY = 'Friday',
+  SATURDAY = 'Saturday',
+  SUNDAY = 'Sunday',
+}
+
+export enum TimetableStatus {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+}
