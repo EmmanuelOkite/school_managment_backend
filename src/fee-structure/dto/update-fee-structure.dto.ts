886eff1f82fb0a0f5757968303797e545d1c0872
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-29 11:08:47 +0300

    added fee strucutre update dto file

---
+++ src/fee-structure/dto/update-fee-structure.dto.ts
@@ -0,0 +1,3 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateFeeStructureDto } from './create-fee-structure.dto';
+export class UpdateFeeStructureDto extends PartialType(CreateFeeStructureDto) {}
