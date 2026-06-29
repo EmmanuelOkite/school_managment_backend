6c0737b8a16bf421d5d8bd30ab2d13ac8a61ee49
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-29 11:24:02 +0300

    added expense enums file

---
+++ src/expense/enums/expense.enum.ts
@@ -0,0 +1,24 @@
+export enum ExpenseCategory {
+  SALARIES = 'Salaries',
+  UTILITIES = 'Utilities',
+  TRANSPORT = 'Transport',
+  MAINTENANCE = 'Maintenance',
+  STATIONERY = 'Stationery',
+  FOOD = 'Food',
+  SPORTS = 'Sports',
+  EXAMINATION = 'Examination',
+  LIBRARY = 'Library',
+  OTHER = 'Other',
+}
+
+export enum ExpenseStatus {
+  PAID = 'Paid',
+  PENDING = 'Pending',
+}
+
+export enum ExpensePaymentMethod {
+  CASH = 'Cash',
+  MOBILE_MONEY = 'Mobile Money',
+  BANK = 'Bank',
+  CARD = 'Card',
+}
