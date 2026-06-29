71ba47fe5bcc3caf546031cafeac508286936aef
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-29 11:16:52 +0300

    added fee collection module file

---
+++ src/fee-collection/fee-collection.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeeCollection } from './entities/fee-collection.entity';
+import { Student } from '../../student/entities/student.entity';
+import { FeeStructure } from '../fee-structure/entities/fee-structure.entity';
+import { StudentBalance } from '../student-balance/entities/student-balance.entity';
+import { FeeCollectionService } from './fee-collection.service';
+import { FeeCollectionController } from './fee-collection.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([FeeCollection, Student, FeeStructure, StudentBalance])],
+  controllers: [FeeCollectionController],
+  providers: [FeeCollectionService],
+  exports: [FeeCollectionService, TypeOrmModule],
+})
+export class FeeCollectionModule {}
