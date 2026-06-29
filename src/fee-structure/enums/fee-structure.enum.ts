8e375f74226c0ea7fccab69d4ea33035d06664cb
EmmanuelOkite <emmanuelokite3@gmail.com>
2026-06-29 11:06:12 +0300

    added fee strucutres enums file

---
+++ src/fee-structure/enums/fee-structure.enum.ts
@@ -0,0 +1,23 @@
+export enum FeeCategory {
+  TUITION = 'Tuition',
+  BOARDING = 'Boarding',
+  UNIFORM = 'Uniform',
+  BOOKS = 'Books',
+  TRANSPORT = 'Transport',
+  MEDICAL = 'Medical',
+  ACTIVITY = 'Activity',
+  EXAMINATION = 'Examination',
+  OTHER = 'Other',
+}
+
+export enum FeeStructureStatus {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+}
+
+export enum Currency {
+  UGX = 'UGX',
+  USD = 'USD',
+  KES = 'KES',
+  TZS = 'TZS',
+}
